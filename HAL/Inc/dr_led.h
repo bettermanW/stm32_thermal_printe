0c19c71b34044068b836028d8dc206d7bd7ff993
wen <1441916502@qq.com>
2025-09-14 18:12:04 +0800

    feat(led) 添加LED四种状态和LE D状态函数
    - 待机（暗） 蓝牙连接（亮）运行异常（快闪）打印中（慢闪）
    - 咱采用阻塞的HAL_Delay

--- HAL/Inc/dr_led.h
+++ HAL/Inc/dr_led.h
@@ -5,4 +5,22 @@
 #ifndef MINIPRINTER_DR_LED_H
 #define MINIPRINTER_DR_LED_H
 
+
+#include "common.h"
+
+/**
+ * 状态灯
+ * 待机   暗
+ * 蓝牙连接 亮
+ * 运行异常 快闪
+ * 打印中  慢闪
+ */
+typedef  enum {
+    WAIT,
+    CONNECTING,
+    WRONG,
+    PRINTING,
+}led_state;
+
+void led_run_state(led_state state);
 #endif //MINIPRINTER_DR_LED_H
--- HAL/Src/dr_led.c
+++ HAL/Src/dr_led.c
@@ -1,3 +1,36 @@
 //
 // Created by 温厂长 on 2025/9/14.
 //
+#include "dr_led.h"
+
+
+
+static void led_flash(const int ms) {
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+    HAL_Delay(ms);
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+    HAL_Delay(ms);
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+    HAL_Delay(ms);
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+    HAL_Delay(ms);
+}
+
+void led_run_state(const led_state state) {
+    switch (state) {
+        case WAIT:
+            HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET); // 灭
+            break;
+        case CONNECTING:
+            HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET); // 常亮
+            break;
+        case WRONG:
+            led_flash(200);
+            break;
+        case PRINTING:
+            led_flash(1000);
+            break;
+        default:
+            break;
+    }
+}
