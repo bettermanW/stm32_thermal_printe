f34da40395f6f975fed7542f7a06708bb7ae33f6
wen <1441916502@qq.com>
2025-09-21 10:48:50 +0800

    优化名称

--- SYSTEM/Src/sys_timer.c
+++ SYSTEM/Src/sys_timer.c
@@ -11,9 +11,9 @@ bool printer_timeout = false;   // 打印超时标志
 
 
 /* read_state_timer_callbackfun function */
-void read_state_timer_callbackfun(void const * argument)
+void read_state_timer_callback_fun(void const * argument)
 {
-    /* USER CODE BEGIN read_state_timer_callbackfun */
+
     // 每次触发时打印提示
     printf("read_state now...\r\n");
     read_state_timeout = true;
@@ -21,7 +21,7 @@ void read_state_timer_callbackfun(void const * argument)
 }
 
 /*打印超时回调函数*/
-void read_timeout_timer_callbackfun(void const * argument)
+void read_timeout_timer_callback_fun(void const * argument)
 {
     printf("触发打印超时错误...\r\n");
     printer_timeout = true;
@@ -29,7 +29,7 @@ void read_timeout_timer_callbackfun(void const * argument)
 
 void init_timer(){
     // 定义一个定时器，名字叫 myStateTimer，回调函数是 read_state_timer_callbackfun
-    osTimerDef(myStateTimer, read_state_timer_callbackfun);
+    osTimerDef(myStateTimer, read_state_timer_callback_fun);
 
     // 创建定时器，模式是周期性 (osTimerPeriodic)，参数为 NULL
     myStateTimerHandle = osTimerCreate(osTimer(myStateTimer), osTimerPeriodic, NULL);
@@ -40,7 +40,7 @@ void init_timer(){
 
 void open_printer_timeout_timer(){
     printer_timeout = false;
-    osTimerDef(myTimeoutTimer, read_timeout_timer_callbackfun);
+    osTimerDef(myTimeoutTimer, read_timeout_timer_callback_fun);
     myTimeoutTimerHandle = osTimerCreate(osTimer(myTimeoutTimer), osTimerOnce, NULL);
     osTimerStart(myTimeoutTimerHandle,20000);   // 20ms
 }
